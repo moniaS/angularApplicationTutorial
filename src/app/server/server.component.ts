de6ba42666bdad727e18cb146e31e37adb42dfef
mstobieniecka <mstobieniecka1@gmail.com>
2017-10-22 21:03:19 +0200

    Added angular 4 tutorial application

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styles: ['.online {color: white;}']
+})
+export class ServerComponent {
+    serverId: number = 1;
+    serverStatus = 'offline'
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online': 'offline';
+    }
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus == 'online'? 'red': 'green';
+    }
+}
